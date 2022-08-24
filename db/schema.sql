8396118863058eb68d8204ae45602ed4f832ac74
Demi Hayashi <demi.h@me.com>
2022-08-23 21:25:08 -0700

    successfully update role table to reference department table

--- db/schema.sql
+++ db/schema.sql
@@ -3,19 +3,20 @@ DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL(10, 2),
-    department_id INTEGER
+    salary DECIMAL(10,2),
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INTEGER,
