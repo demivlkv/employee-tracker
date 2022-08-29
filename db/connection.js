4be588137784a39f966a46aebc738e1dfa022990
Demi Hayashi <demi.h@me.com>
2022-08-29 15:42:49 -0700

    update connection

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
+require('dotenv').config();
 
 // connect to database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'Tvoisyabr06.',
-        database: 'companyDB'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the company database!')
 );
